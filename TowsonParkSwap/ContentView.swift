8f31655f65e083875432149e38bc49381af1c0ed
below2 <112999552+below2@users.noreply.github.com>
2022-11-15 22:15:39 -0500

    Authentication implemented
    ContentView: Almost a complete revamp to support login and authentication using Auth0
    ProfileView: New view which will over take ParkerSwapperView as it shows details of the user account as well as lets them choose their role
    Profile: New object, name is explanatory

--- TowsonParkSwap/ContentView.swift
+++ TowsonParkSwap/ContentView.swift
@@ -6,131 +6,110 @@
 //
 
 import SwiftUI
-// Global delcarations:
-
-// Global color object because used in login text fields
-let lightGrayColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
-
-// Sample username and pass (TODO: implement auth0 to authenticate registration and login)
-let storedUsername = "Admin"
-let storedPassword = "pass"
+import Auth0
 
 struct ContentView: View {
     
     // State property vars (state properties persistently update view):
-    @State var username: String = ""
-    @State var password: String = ""
-    @State var authenticationFail: Bool = false
-    // SET TO TRUE TO BYPASS LOGIN FOR DEBUGGING
     @State var authenticationPass: Bool = true
-    @State var path = NavigationPath()
+    @State var userProfile = Profile.empty
     
     var body: some View {
-        NavigationStack(path: $path) {
-            // Stacks elements across Z-axis (unused right now)
-            ZStack{
-                // Stacks elements V-axis
-                VStack {
-                    
-                    // Visual elements (extracted to subviews)
-                    WelcomeText()
-                    AppLogo()
-                    UsernameTextField(username: $username)
-                    PasswordSecureField(password: $password)
-                    
-                    // If login fail, display text
-                    if authenticationFail {
-                        Text("Login failed")
-                            .offset(y: -10)
-                            .foregroundColor(.red)
-                    }
-                    
-                    // If login pass, display NavigationLink
-                    if authenticationPass {
-                        NavigationLink("Login", destination: ParkerSwapperView())
-                    }
-                    
-                    // Button (logic and action)
+        // Necessary to navigate to next view
+        NavigationView {
+            
+            // Stacks elements V-axis
+            VStack {
+                
+                // Visual elements (extracted to subviews)
+                WelcomeText()
+                AppLogo()
+                
+                if authenticationPass == false {
                     Button(action: {
-                        if self.username == storedUsername && self.password == storedPassword {
-                            self.authenticationPass = true
-                            self.authenticationFail = false
-                        } else {
-                            self.authenticationPass = false
-                            self.authenticationFail = true
-                        }
+                        login()
                     }) {
-                        // Button visual
-                        LoginButtonContent()
+                        Text("Login")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 280.0, height: 80)
+                            .background(Color.yellow)
+                            .cornerRadius(15.0)
+                    }
+                } else {
+                    NavigationLink("Continue", destination: ProfileView(userProfile: userProfile))
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 280.0, height: 80)
+                        .background(Color.yellow)
+                        .cornerRadius(15.0)
+                    
+                    Button(action: {
+                        logout()
+                    }) {
+                        Text("Log out")
                     }
                 }
-                // Padding for VStack itself, saw that you should do this online, but unsure why
-                .padding()
             }
         }
     }
 }
-
-// EXTRACTED FUNCTIONS
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    // EXTRACTED FUNCTIONS
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
-
-// Welcome text and design
-struct WelcomeText: View {
-    var body: some View {
-        Text("Welcome!")
-            .font(.largeTitle)
-            .fontWeight(.semibold)
-            .padding(.bottom, 20)
+    
+    // Welcome text and design
+    struct WelcomeText: View {
+        var body: some View {
+            Text("Welcome!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 20)
+        }
     }
-}
-
-// Login button text and design
-struct LoginButtonContent: View {
-    var body: some View {
-        Text("LOGIN")
-            .font(.headline)
-            .foregroundColor(.white)
-            .padding()
-            .frame(width: 220, height: 60)
-            .background(Color.yellow)
-            .cornerRadius(15.0)
+    
+    // Towson logo calling from assets folder (TODO: crop, change to custom logo)
+    struct AppLogo: View {
+        var body: some View {
+            Image("towson_university_logo")
+                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                .aspectRatio(contentMode: .fit)
+                .padding(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/)
+        }
     }
-}
-
-// Towson logo calling from assets folder (TODO: crop, change to custom logo)
-struct AppLogo: View {
-    var body: some View {
-        Image("towson_university_logo")
-            .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
-            .aspectRatio(contentMode: .fit)
-            .padding(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/)
+    
+    extension ContentView {
+        func login() {
+            Auth0
+                .webAuth()
+                .start { result in
+                    switch result {
+                    case .failure(let error):
+                        print("Failed with: \(error)")
+                    case .success(let credentials):
+                        self.authenticationPass = true
+                        self.userProfile = Profile.from(credentials.idToken)
+                    }
+                }
+        }
+        
+        func logout() {
+            Auth0
+                .webAuth()
+                .clearSession { result in
+                    switch result {
+                    case .failure(let error):
+                        print("Failed with: \(error)")
+                    case .success:
+                        self.authenticationPass = false
+                        self.userProfile = Profile.empty
+                    }
+                }
+        }
     }
-}
-
-// Username text field and design (bound to state var $username and design)
-struct UsernameTextField: View {
-    @Binding var username: String
-    var body: some View {
-        TextField("Username", text: $username)
-            .padding()
-            .background(lightGrayColor)
-            .cornerRadius(5.0)
-            .padding(.bottom, 20)
-    }
-}
-
-// Password secure field and design (bound to state var $password and design)
-struct PasswordSecureField: View {
-    @Binding var password: String
-    var body: some View {
-        SecureField("Password", text: $password)
-            .padding()
-            .background(lightGrayColor)
-            .cornerRadius(5.0)
-            .padding(.bottom, 20)
-    }
-}
---
+++ TowsonParkSwap/ProfileView.swift
@@ -0,0 +1,153 @@
+//
+//  ProfileView.swift
+//  TowsonParkSwap
+//
+//  Created by user229386 on 11/15/22.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    
+    // Passed property var from ContentView
+    var userProfile: Profile
+    
+    // State property vars:
+    @State var parkerSelected: Bool = false
+    @State var swapperSelected: Bool = false
+    
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            // User image
+            UserImage(urlString: userProfile.picture)
+            
+            // User details (TODO: add car details linked to user account)
+            VStack {
+                Text("Name: \(userProfile.name)")
+                    .font(.title)
+                Text("Email: \(userProfile.email)")
+                    .font(.title)
+            }
+            
+            Spacer()
+            
+            // Role selection options
+            Group {
+                
+                Text("Select role:")
+                    .font(.title3)
+                
+                HStack {
+                    
+                    // Parker button
+                    Button(action: {
+                        parkerSelected.toggle()
+                        swapperSelected = false
+                    }) {
+                        Text("PARKER")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 180.0, height: 100)
+                            .background(Color.yellow)
+                            .cornerRadius(15.0)
+                    }
+                    
+                    // Swapper button
+                    Button(action: {
+                        swapperSelected.toggle()
+                        parkerSelected = false
+                    }) {
+                        Text("SWAPPER")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 180.0, height: 100)
+                            .background(Color.yellow)
+                            .cornerRadius(15.0)
+                    }
+                }
+                
+                Spacer()
+                
+                // NavigationLink logic for parker
+                if parkerSelected {
+                    // Gray divider line
+                    Divider()
+                        .frame(width: 360.0)
+                        .background(Color.gray)
+                        .offset(y: -30)
+                    
+                    Text("Parker role selected")
+                        .font(.title3)
+                    
+                    NavigationLink("Continue", destination: ParkerView())
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .frame(width: 280.0, height: 60.0)
+                        .background(Color.yellow)
+                        .cornerRadius(10.0)
+                }
+                
+                // NavigationLink logic for swapper
+                else if swapperSelected {
+                    // Gray divider line
+                    Divider()
+                        .frame(width: 360.0)
+                        .background(Color.gray)
+                        .offset(y: -30)
+                    
+                    Text("Swapper role selected")
+                        .font(.title3)
+                    
+                    NavigationLink("Continue", destination: SwapperView())
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .frame(width: 280.0, height: 60.0)
+                        .background(Color.yellow)
+                        .cornerRadius(10.0)
+                }
+                
+            }
+        }
+        .padding()
+    }
+}
+
+struct UserImage: View {
+    // Given the URL of the user’s picture, this view asynchronously
+    // loads that picture and displays it. It displays a “person”
+    // placeholder image while downloading the picture or if
+    // the picture has failed to download.
+    
+    var urlString: String
+    
+    var body: some View {
+        AsyncImage(url: URL(string: urlString)) { image in
+            image
+                .frame(maxWidth: 128)
+        } placeholder: {
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 128)
+                .foregroundColor(.blue)
+                .opacity(0.5)
+        }
+        .padding(20)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static let userProfilePreview = Profile.empty
+    
+    static var previews: some View {
+        ProfileView(userProfile: userProfilePreview)
+    }
+}
+    
+    
+
